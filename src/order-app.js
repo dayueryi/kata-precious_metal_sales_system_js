9cc90ffa0903405f709adaaff9cf8a84dec23d3a
wangjiawei <wangjiawei_nj@csii.com.cn>
2019-07-02 20:44:23 +0800

    add:计算商品订单总价 应付金额 优惠金额

--- src/order-app.js
+++ src/order-app.js
@@ -21,17 +21,26 @@ export default class OrderApp {
       let goodsDisconut = new GoodsDisconut(item.product, item.amount, orderInfo.discountCards);
       return goodsDisconut.getGoodsMessage();
     });
-    //  构造订单详情和优惠详情的字符串
+    //  构造订单详情和优惠详情的字符串 获取商品总金额和实际金额
     let orderItemList = [];
     let discountItemList = [];
+    let totalPrice = 0;
+    let discountPrice = 0;
     goodsList.forEach(item => {
+      totalPrice = totalPrice + item.price;
       orderItemList.push(new OrderItem({productNo: item.productNo, productName: item.productName, price: item.price, amount: item.amount, subTotal:item.subTotal}));
       // 如果存在优惠，则构造优惠信息
       if(item.discount && item.discount > 0){
+        discountPrice = discountPrice + item.discount;
         discountItemList.push(new DiscountItem({productNo: item.productNo, productName: item.productName,discount: -item.discount}));
       }
     });
-
-    return (new OrderRepresentation({})).toString();
+    let payments = [{
+      type: orderInfo.payments[0].type,
+      amount: totalPrice - discountPrice
+    }];
+    
+    
+    return (new OrderRepresentation({data})).toString();
   }
 }
