0375d25e739f065732bd49c2016f28a29a1e3fca
沈颖杰 <shenyingjie@shenyingjiedeMacBook-Pro.local>
2019-07-02 18:54:33 +0800

    修改优惠规则描述

--- src/order-app.js
+++ src/order-app.js
@@ -1,23 +1,8 @@
 import OrderRepresentation from './output/order-representation';
-import { rules, goods, mumberType, userInfo } from './../test/resources/commom.json';
-
-
-
-
 export default class OrderApp {
 
   checkout(orderCommand) {
     // TODO: 请完成需求指定的功能
-    // 获取用户
-    let orderInfo = JSON.parse(orderCommand);
-    let user = userInfo.find((item) => {
-      return item.cardNumber === orderInfo.memberId;
-    });
-    // 获取订单商品
-    let goodsList = orderCommand.items;
-    // 获取订单使用优惠劵的信息
-
-
     return (new OrderRepresentation({})).toString();
   }
 }
